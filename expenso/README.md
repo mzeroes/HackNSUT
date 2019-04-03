9b30771ef9ebf84b30c465f51cba09d3f3c12b02
Shubham Jain <s.shubjain@gmail.com>
2019-04-03 20:19:49 +0530

    Added Readme and Info

---
+++ expenso/README.md
@@ -0,0 +1,9 @@
+## Expenso
+
+## **Project Info**
+Expenso helps you manages your expenses by keeping record of your daily expenses. You get a statistical overview of your income and expense for the past week or month. It can also be used by students to manage their expenses as well as employees to keep track of money they spend each day. You can add income on a regular basis such as salary. We made this solution because of the problem mentioned below. This is a problem most of us face, atleast I do. I don't keep in mind the amount of Money I have. I usually spend it and forget about it.
+
+## **Technology Used**
+- React Native (Expo)
+- Optical Character Recognition and Keyword extraction
+- Firebase
--- expenso/config/keys.js
+++ expenso/config/keys.js
@@ -1,16 +1,16 @@
 export const firebaseConfig = {
-  apiKey: 'AIzaSyCUy51RQvU9pckGiKZ4sZ7ytnji25Soa04',
-  authDomain: 'gottfried-leibniz.firebaseapp.com',
-  databaseURL: 'https://gottfried-leibniz.firebaseio.com',
-  projectId: 'gottfried-leibniz',
-  storageBucket: 'gottfried-leibniz.appspot.com',
-  messagingSenderId: '689176639770',
-  hostingURL: 'https://gottfried-leibniz.firebaseapp.com'
+  apiKey: 'AIzaSyAkwuyg9_5-e9bmJEQwER5smD1yEydlmhQ',
+  authDomain: 'expenso-app.firebaseapp.com',
+  databaseURL: 'https://expenso-app.firebaseio.com',
+  projectId: 'expenso-app',
+  storageBucket: 'expenso-app.appspot.com',
+  messagingSenderId: '663745175868',
+  hostingURL: 'expenso-app.firebaseapp.com'
 };
 
 export const authConfig = {
   GOOGLE_CLIENT_ID:
-    '689176639770-tokjfdm46rnlepnna7kbedd674pgum2j.apps.googleusercontent.com'
+    '663745175868-img3indplmi1757j0dl7iv667nssbm92.apps.googleusercontent.com'
 };
 
 export const API_KEY = 'rksLL7EMibN1tDdoEM3eDyc2gM95uHl1OUYzcs1GnPwChttxbwrj9hOo1rhUwlcJ';
